5ac12a33b9d8c572f7c82e5865394b2d9fdc4d34
Timothy Saudners <timsaunders88@gmail.com>
2020-08-21 18:46:32 +0100

    added calculateRanking function content to calculate rank based on comparing instructor votes

--- instructor.js
+++ instructor.js
@@ -5,11 +5,18 @@ const Instructor = function(firstName, lastName, speciality, hasPet){
     this.speciality = speciality;
     this.hasPet = hasPet;
     this.votes = 0;
-    this.ranking = calculateRanking();
+    this.ranking = calculateRanking(this.votes);
 };
 
 // must be named function to allow hoisting into constructor
     // but this means it can't be a prototype and called as an object method later on. This is frustrating...
-function calculateRanking() {
-    return 1;
+function calculateRanking(votes) {
+    const rank = instructors.filter((instructor) => {
+        return instructor.votes > votes;
+    }).length + 1;
+    return rank;
   };
+
+Instructor.prototype.recalculateRank = () => {
+    return calculateRanking(this.votes);
+}
