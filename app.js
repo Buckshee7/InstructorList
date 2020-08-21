c635d1f93494d53f33cdc9e4ebb2f2633981c621
Timothy Saudners <timsaunders88@gmail.com>
2020-08-21 15:11:13 +0100

    refactored handleSubmit into smaller helper functions

--- app.js
+++ app.js
@@ -1,10 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const submitButton = document.querySelector('#submit-button');
-    submitButton.addEventListener('click', handleSubmit);
+    const form = document.querySelector('#new-instructor');
+    form.addEventListener('submit', handleSubmit);
 
 });
 
 const instructors = [];
+
+const createNewInstructor = (event) => {
+    // create newInstructor and add to instructors list
+
+    let hasPet = undefined;
+    if (event.target.hasPetYes === false){
+        haspet = false;
+    } else{
+        hasPet = true;
+    };
+
+    const newInstructor = new Instructor(
+        event.target.firstName.value, 
+        event.target.lastName.value,
+        event.target.speciality.value,
+        hasPet);
+    instructors.push(newInstructor);
+
+    return newInstructor;
+};
+
+const createNewListItem = (newInstructor) => {
+    // // create li
+    const listElement = document.createElement('li')
+
+    // // create li elemesnts
+    const name = document.createElement('h3');
+    const speciality = document.createElement('p');
+    const pet = document.createElement('p');
+    const ranking = document.createElement('p');
+    const deleteButton = document.createElement('button')
+    const voteButton = document.createElement('button')
+
+    // // add values to li elements
+    name.textContent = `Name: ${newInstructor.firstName} ${newInstructor.lastName}`;
+    speciality.textContent = `Speciality: ${newInstructor.speciality}`;
+    pet.textContent = `Has Pet(s): ${newInstructor.hasPet}`;
+    ranking.textContent = `Instructor Ranking: ${newInstructor.ranking}`;
+    deleteButton.textContent = "Delete Instructor";
+    deleteButton.addEventListener('click', handleDeleteSingle);
+    voteButton.textContent = "Upvote Instructor";
+    voteButton.addEventListener('click', handleVote)
+
+    // // add elements to li
+    listElement.append(name) 
+    listElement.append(speciality) 
+    listElement.append(pet) 
+    listElement.append(ranking) 
+    listElement.append(voteButton) 
+    listElement.append(deleteButton) 
+
+    return listElement;
+}
 
 const handleDeleteSingle = () => {
   return "okay"
@@ -16,58 +69,15 @@ const handleVote = () => {
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event)
 
-    let hasPet = undefined;
-    if (event.target.form.hasPetYes === false){
-        haspet = false;
-    } else{
-        hasPet = true;
-    };
-    
-    const newInstructor = new Instructor(
-        event.target.form.firstName.value, 
-        event.target.form.lastName.value,
-        event.target.form.speciality.value,
-        hasPet);
-    
-    //create new list item
-        // define parent element
-        const parentNode = document.querySelector('#instructors-list')
+    const newInstructor = createNewInstructor(event);
 
-        // create li
-        const listElement = document.createElement('li')
+    const listItem = createNewListItem(newInstructor);
 
-        // create li elements
-        const name = document.createElement('h3');
-        const speciality = document.createElement('p');
-        const pet = document.createElement('p');
-        const ranking = document.createElement('p');
-        const deleteButton = document.createElement('button')
-        const voteButton = document.createElement('button')
+    // add li to parent node
+    const parentNode = document.querySelector('#instructors-list');
+    parentNode.append(listItem);
 
-        // add values to li elements
-        name.textContent = `Name: ${newInstructor.firstName} ${newInstructor.lastName}`;
-        speciality.textContent = `Speciality: ${newInstructor.speciality}`;
-        pet.textContent = `Has Pet(s): ${newInstructor.hasPet}`;
-        ranking.textContent = `Instructor Ranking: ${newInstructor.ranking}`;
-        deleteButton.textContent = "Delete Instructor";
-        deleteButton.addEventListener('click', handleDeleteSingle);
-        voteButton.textContent = "UpVote Instructor";
-        voteButton.addEventListener('click', handleVote)
-
-        // add elements to li
-        listElement.append(name) 
-        listElement.append(speciality) 
-        listElement.append(pet) 
-        listElement.append(ranking) 
-        listElement.append(voteButton) 
-        listElement.append(deleteButton) 
-
-        // add li to parent
-        parentNode.append(listElement)
-
-
-        console.log(newInstructor)
-}
+    event.target.reset();
+};
 
