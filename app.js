edc7325fadc5aa6236b194c7bd0b9b190b4e9906
Timothy Saudners <timsaunders88@gmail.com>
2020-08-21 14:47:07 +0100

    added handleSubmit callback function - creates new list element

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+const instructors = [];
+
+const handleDeleteSingle = () => {
+  return "okay"
+}
+
+const handleVote = () => {
+    return "okay"
+  }
+
 const handleSubmit = (event) => {
     event.preventDefault();
     console.log(event)
@@ -21,5 +31,43 @@ const handleSubmit = (event) => {
         event.target.form.speciality.value,
         hasPet);
     
+    //create new list item
+        // define parent element
+        const parentNode = document.querySelector('#instructors-list')
+
+        // create li
+        const listElement = document.createElement('li')
+
+        // create li elements
+        const name = document.createElement('h3');
+        const speciality = document.createElement('p');
+        const pet = document.createElement('p');
+        const ranking = document.createElement('p');
+        const deleteButton = document.createElement('button')
+        const voteButton = document.createElement('button')
+
+        // add values to li elements
+        name.textContent = `Name: ${newInstructor.firstName} ${newInstructor.lastName}`;
+        speciality.textContent = `Speciality: ${newInstructor.speciality}`;
+        pet.textContent = `Has Pet(s): ${newInstructor.hasPet}`;
+        ranking.textContent = `Instructor Ranking: ${newInstructor.ranking}`;
+        deleteButton.textContent = "Delete Instructor";
+        deleteButton.addEventListener('click', handleDeleteSingle);
+        voteButton.textContent = "UpVote Instructor";
+        voteButton.addEventListener('click', handleVote)
+
+        // add elements to li
+        listElement.append(name) 
+        listElement.append(speciality) 
+        listElement.append(pet) 
+        listElement.append(ranking) 
+        listElement.append(voteButton) 
+        listElement.append(deleteButton) 
+
+        // add li to parent
+        parentNode.append(listElement)
+
+
         console.log(newInstructor)
 }
+
